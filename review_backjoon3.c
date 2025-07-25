dcb9df644b0025a4ee28c54342a7f00418d65ad3
difficcd <144074124+difficcd@users.noreply.github.com>
2025-07-25 21:04:45 +0900

    0725

--- review_backjoon3.c
+++ review_backjoon3.c
@@ -1,5 +1,5 @@
 https://www.acmicpc.net/problem/11279 
- 0725 최대 힙
+ 0726 최대 힙
 https://www.acmicpc.net/problem/1158
   요세푸스 => 0724 ok
    + hwp 참고 & 프로젝트 진행
@@ -64,3 +64,126 @@ int main() {
   cout << j[N-1] << ">";
   
 }
+
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main() {
+
+  // 2675: 문자열 반복
+  int T, R;
+  cin >> T;
+  
+  string S;
+
+  for (int i=0; i<T; i++){
+    cin >> R >> S;
+
+    for(int j=0; j<; j++)
+      for(int k=0; k<R; k++)
+        cout << S[j];
+    
+    cout <<"\n";
+  }
+
+
+  // 10809: 알파벳 찾기
+  
+  string str;
+  int alphabet[26];
+
+  for(int i=0; i<26; i++)
+   alphabet[i] = -1;
+
+  
+  getline(cin, str, '\n');
+  
+  for(int i=0; i<str.length(); i++){
+   if(alphabet[int(str[i])-97] == -1)
+    alphabet[int(str[i])-97] = i;
+  }
+
+  for(int i=0; i<26; i++)
+    cout << alphabet[i] << " ";
+
+  
+}
+
+// 1436번 : 영화감독 숌
+
+#include <iostream>
+#include <string>
+#define MAX 1000000
+using namespace std;
+
+int main() {
+
+  int arr[MAX]; 
+  int N=0;
+  int temp=0;
+
+   cin >> N; 
+  
+  for(int i=666; i<MAX; i++){
+    string str = to_string(i);
+
+    // 해당 숫자의 666개수여부 count
+                           
+    for(int j=0; j<str.length()-2; j++)
+      if(str.substr(j, 3) == "666"){
+        arr[temp] = i;
+        temp++;
+        break;
+      }
+      
+  }
+
+  cout << arr[N-1];
+
+  
+}
+
+// 배열은 용량 문제로 백준에서 틀렸습니다 처리.
+// 아래는 개선 코드
+#include <iostream>
+#include <string>
+#define MAX 1000000
+using namespace std;
+
+int main() {
+
+  int N;
+  int count = 0;
+  int num = 666;
+
+  int temp=1;
+
+   cin >> N; 
+
+  while(temp){
+    string str = to_string(num);
+    
+    for(int j=0; j<str.length()-2; j++)
+      if(str.substr(j, 3) == "666"){
+        count++; // 몇 번째 666인지 나타냄
+        if(count == N){
+          cout << num;
+          temp = 0;
+          break;
+          }
+        break;
+      }
+          
+    num++;
+  }
+
+
+}
+
+
+
+
