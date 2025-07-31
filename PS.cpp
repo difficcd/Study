93301fdebcbad2ae466a1d9952f4555f2748f038
difficcd <144074124+difficcd@users.noreply.github.com>
2025-07-31 20:25:30 +0900

    0731

--- PS.cpp
+++ PS.cpp
@@ -4,3 +4,59 @@ https://steady-coding.tistory.com/260
 ps 공부방법
 
   https://www.acmicpc.net/board/view/116825
+
+// 분할정복 : 재귀 하노이 탑
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+vector<int> v[3];
+vector<int> result;
+int count=0;
+
+// 각 단계를 출력해야 함
+void hanoi_top(int N, int from, int to, int via){
+    if(N==1) {
+        // from => to 로 N 이동
+        v[to].push_back(v[from].back()); // 수 저장
+        v[from].pop_back(); // 제거
+        result.push_back(from+1); 
+        result.push_back(to+1);
+        count ++;
+    }
+    else {
+        hanoi_top(N-1, from, via, to); // 위의값 임시로 옮김
+        
+        v[to].push_back(v[from].back()); // 수 저장
+        v[from].pop_back(); // 제거
+        result.push_back(from+1); 
+        result.push_back(to+1);
+        
+        count ++;
+        hanoi_top(N-1, via, to, from); // 옮긴 값 복원
+        // N-1 을 via => to 로 이동
+    }
+
+}
+
+int main() {
+    int N;
+    cin >> N;
+
+    for(int i=N; i>=1; i--){
+        v[0].push_back(i);
+    }
+    
+    hanoi_top(N,0,2,1);
+    
+    cout << count << "\n";
+
+    for(int i=0; i<result.size(); i++){
+        if((i+1)%2 != 0)cout << result[i] << " ";
+        else cout << result[i];
+        if((i+1)%2 == 0 && i != result.size())
+            cout << "\n";
+    }
+}
+
