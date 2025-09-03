f3592fd6a1be3ee9c8d664f02c1acfbbe2159fdb
difficcd <144074124+difficcd@users.noreply.github.com>
2025-09-03 11:51:52 +0900

    0902

--- PS_04.cpp
+++ PS_04.cpp
@@ -221,6 +221,115 @@ int main(){
   // 상하좌우가 인접 정점 list 임
   // 1이 들어온 위치를 기억해서 모두 인자로 보내야함
 
-  
 }
+
+
+// 2206 벽 부수고 이동하기
+#include <iostream>
+#include <vector>
+#include <tuple>
+#include <queue>
+#include <string>
+using namespace std;
+
+int N, M;
+
+vector<tuple<int, int,int>> adjacent(tuple<int, int, int> v){
+
+  int x = get<0>(v); 
+  int y = get<1>(v);
+  int z = get<2>(v); // 상태 보존 필요
+  // >> broken = 1 된 이후로는 계속 1인 상태로 큐에 들어가줘야 함
+
+  vector<tuple<int, int, int>> t;
+
+  if( x+1 < N ) t.push_back({x+1,y,z});
+  if( x-1 >= 0) t.push_back({x-1,y,z});
+  if (y+1 < M) t.push_back({x,y+1,z});
+  if (y-1 >= 0) t.push_back({x,y-1,z});
+
+  return t;
+}
+
+
+int BFS_wall(vector<vector<int>> map, vector<vector<vector<int>>> &visited){
+
+  queue<tuple<int, int, int>> q;
+  q.push({0, 0, 0});
+  visited[0][0][0] = 1; 
+
+  while(!q.empty()){
+    tuple<int, int, int> v = q.front();
+    q.pop();
+
+    int x = get<0>(v);
+    int y = get<1>(v);
+    int z = get<2>(v); 
+
+    for(tuple<int, int, int> i : adjacent(v)){
+      int nx = get<0>(i);
+      int ny = get<1>(i);
+      int nz = get<2>(i); // 인접정점의 원래 broken 여부
+
+      if(!map[nx][ny] && !visited[nx][ny][z]){ // 길 존재 & 방문안함
+        visited[nx][ny][z] = visited[x][y][z]+1; // map 방문
+        // * 횟수 count 를 위해 level을 visited 값으로 둬야 함
+        // * visited(인접정점의 x,y &  기존정점의 z) = 기존정점의 visited 값+1 (level)
+        q.push({nx,ny,z}); 
+      }
+
+      if(map[nx][ny] && !nz) { // 벽 & 부순적 없음
+        visited[nx][ny][1] = visited[x][y][z]+1;
+        // * visited(인접정점의 x,y, 방문되었으니 z=1 상수로 지정) = ...
+        q.push({nx,ny,1});
+      }
+
+    }
+
+  }
+
+  // result를 큐 전체에서 max로 갱신하지 말고
+  // 도착점(N-1,M-1)의 visited[x][y][0/1] 중 최소를 선택해야 정확함
+
+  int c0 = visited[N-1][M-1][0];
+  int c1 = visited[N-1][M-1][1];
+
+  if(c0 && c1) return min(c0,c1);
+  else if(c0) return c0;
+  else if(c1) return c1;
+  else return -1;
+
+}
+
+
+
+
+
+int main(){
+  cin >> N >> M;
+
+
+  vector<vector<int>> map(N, vector<int>(M, 0));
+  vector<vector<vector<int>>> visited(N, vector<vector<int>>(M, vector<int>(2, 0)));
+
+  /*
+  visited[x][y][0] : 벽 안 부수고 (x, y) 도착했을 때 방문 여부
+  visited[x][y][1] : 벽 부수고 (x, y) 도착했을 때 방문 여부
+  */
+
+  for(int i=0; i<N; i++){
+    string str; cin >> str;
+    for (int j=0; j<M; j++) {
+        int digit = str[j] - '0'; 
+        map[i][j] = digit;
+    }  
+  }
+
+  int result = BFS_wall(map, visited);
+
+  if(visited[N-1][M-1][0] || visited[N-1][M-1][1]) cout << result << "\n";
+  else cout << "-1";
+
+}
+
 
