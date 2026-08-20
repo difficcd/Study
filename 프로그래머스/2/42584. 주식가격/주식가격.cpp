611c232887bda9660a953bc6c2fc37913ef39919
difficcd <leeow1214@gmail.com>
2026-08-20 22:39:00 +0900

    [level 2] Title: 주식가격 -PS Push

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> prices) {
+    vector<int> answer;
+    
+    for(int i=0; i<prices.size(); i++){
+        int temp = prices[i];
+        int cnt = 0;
+        
+        for(int j=i+1; j<prices.size(); j++){
+            if(temp <= prices[j]) cnt++;
+            else {
+                cnt++; 
+                break;
+            }
+        }
+        
+        answer.push_back(cnt);
+    }
+    
+    return answer;
+}
